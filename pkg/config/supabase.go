09c3225b4bb194cd9c434e0d9636026ed3167560
Abhishek Yadav <intruder9455@gmail.com>
2026-08-13 22:09:19 +0530

    removed log.fatal and added log.Println instead

--- pkg/config/supabase.go
+++ pkg/config/supabase.go
@@ -15,13 +15,13 @@ type Client struct{
 func NewSupabaseClient() *Client {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("couldnt load .env file:", err)
+		log.Println("couldnt load .env file:", err)
 	}
 	supabase_url := os.Getenv("SUPABASE_URL")
 	// supabase_anon_key := os.Getenv("SUPABASE_ANON_KEY")
 	supabase_service_role_key := os.Getenv("SUPABASE_SERVICE_KEY")
 	if supabase_service_role_key==""{
-		log.Fatal("SUPABASE_SERVICE_KEY is required for server side database writes")
+		log.Println("SUPABASE_SERVICE_KEY is required for server side database writes")
 	}
 
 	client, err := supabase.NewClient(supabase_url, supabase_service_role_key, &supabase.ClientOptions{})
